db5379ebc680c19d870b24a395b193af8c857bb4
pudding <puddingnnn529@gmail.com>
2013-04-21 22:45:23 +0800

    some changes for rda

--- R code/read_files/connect_postgres.R
+++ R code/read_files/connect_postgres.R
@@ -9,11 +9,11 @@ con = dbConnect(drv, dbname = "kdd_sample",
 # 收割结果
 #df = fetch(rs, n = -1)
 # 直接执行查询返回结果
-author1000=dbGetQuery(con, "select * from author1000;")
-paper1000=dbGetQuery(con, "select * from paper1000;")
-conference1000=dbGetQuery(con, "select * from conference1000;")
-journal1000=dbGetQuery(con, "select * from journal1000;")
-paperauthor1000=dbGetQuery(con, "select * from paperauthor1000;")
+author=dbGetQuery(con, "select * from author1000;")
+paper=dbGetQuery(con, "select * from paper1000;")
+conference=dbGetQuery(con, "select * from conference1000;")
+journal=dbGetQuery(con, "select * from journal1000;")
+paperauthor=dbGetQuery(con, "select * from paperauthor1000;")
 trainconfirmed=dbGetQuery(con, "select * from trainconfirmed1000;")
 traindeleted=dbGetQuery(con, "select * from traindeleted1000;")
 validpaper=dbGetQuery(con, "select * from validpaper1000;")
@@ -28,11 +28,11 @@ recoding<-function(vec)
   Encoding(vec)="UTF-8"
   return(vec)
 }
-author=apply(author1000,2,recoding)
-paper=apply(paper1000,2,recoding)
-conference=apply(conference1000,2,recoding)
-journal=apply(journal1000,2,recoding)
-paperauthor=apply(paperauthor1000,2,recoding)
+author[,2:3]=apply(author[,2:3],2,recoding)
+paper[,c(2,6)]=apply(paper[,c(2,6)],2,recoding)
+conference[,2:4]=apply(conference[,2:4],2,recoding)
+journal[,2:4]=apply(journal[,2:4],2,recoding)
+paperauthor[,3:4]=apply(paperauthor[,3:4],2,recoding)
 
 setwd("F:/kdd/2013 kdd/rda")
 save(author,file="author.rda")
@@ -41,8 +41,7 @@ save(conference,file="conference.rda")
 save(journal,file="journal.rda")
 save(paperauthor,file="paperauthor.rda")
 save(trainconfirmed,file="trainconfirmed.rda")
-save(traindeleted,file="traindeleted.rda")
-save(validpaper,file="validpaper.rda")
+
 
 
 
