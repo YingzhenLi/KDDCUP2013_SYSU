458f12c4b4f596252de287aedaf6c2169a74b829
pudding <puddingnnn529@gmail.com>
2013-04-29 23:06:56 +0800

    rollback

--- R code/GetFeatures/keywords.R
+++ R code/GetFeatures/keywords.R
@@ -1,6 +1,56 @@
 
+
+setwd("F:/kdd/2013 kdd/rda")
+load("F:/kdd/2013 kdd/rda/paper.rda")
 keyword=as.character(paper$keyword)
 
+
+
+######trim the key first ###########
+
+key.trim<-function(word)
+{
+  end=nchar(word)+2
+  a=gregexpr("index[\\s-！]*terms",word,perl=T,ignore.case=T)
+  if (a[[1]][1]!=-1)
+    word=substr(word,a[[1]][1]+attr(a[[1]],"match.length")[1]+1,end)
+  
+  a=gregexpr("Key[\\s-！]*words",word,perl=T,ignore.case=T)
+  show(a[[1]][1])
+  if (a[[1]][1]==-1) return(c(word,""))
+  
+  start=a[[1]][1]+attr(a[[1]],"match.length")[1]+1
+  
+  
+  if (a[[1]][1]==1)
+  {
+    return(c(substr(word,start,end),""))
+  }
+  if (a[[1]][1]>1)
+  {
+    b=gregexpr(":",substr(word,start-1,end),perl=T)
+    if (b[[1]][1]==-1) return(c(word,""))
+    else 
+    {
+      mid=start+attr(b[[1]],"match.length")[1]
+      spl=unlist(strsplit(word,substr(word,a[[1]][1]-1,mid),fixed=T))
+      if(length(grep("[a-zA-Z]",spl[1],perl=T))==0) spl[1]=""
+      return(spl[2:1])
+    }
+  }
+}
+
+a=unlist(gregexpr("Key[\\s-！]*words",keyword,perl=T,ignore.case=T))
+b=unlist(gregexpr("index[\\s-！]*terms",keyword,perl=T,ignore.case=T))
+index=which(a>0|b>0)
+kk=lapply(keyword[index],key.trim)
+k=matrix(unlist(kk),ncol=2,byrow=T)
+keyword[index]=k[,1]
+firstkey=rep("",length(keyword))
+firstkey[index]=k[,2]
+
+
+######split the keywords ####
 getLen<-function(vector)
 {
   if (min(vector)==-1) return(0)
@@ -10,17 +60,17 @@ getLen<-function(vector)
 splitkey<-function(keyword)
 {
   s=matrix(0,nrow=length(keyword),ncol=6)
-  s[,1]=unlist(lapply(gregexpr(";\\s",keyword),getLen))
+  s[,1]=unlist(lapply(gregexpr(";",keyword),getLen))
   s[,2]=unlist(lapply(gregexpr(",",keyword),getLen))
   s[,3]=unlist(lapply(gregexpr("\\|",keyword),getLen))
-  s[,4]=unlist(lapply(gregexpr("\\.\\s",keyword),getLen))
+  s[,4]=unlist(lapply(gregexpr("！",keyword),getLen))
   s[,5]=unlist(lapply(gregexpr("，",keyword),getLen))
-  s[,6]=unlist(lapply(gregexpr("-\\s",keyword),getLen))
+  s[,6]=unlist(lapply(gregexpr("[^0-9]\\.[^0-9]",keyword),getLen))
   return(s)
 }
 
 s=splitkey(keyword)
-spl=c(";\\s",",","\\|","\\.\\s","，","-\\s")
+spl=c(";",",","\\|","！","，","\\.")
 spliter=apply(s,1,
               function(x) {
                 if (max(x)==0) return(spl[1])
@@ -36,4 +86,9 @@ pasteind<-function(vector)
 }
 
 ind=apply(s,1,pasteind)
+table(ind[nchar(ind)>=2])
+keyword[ind=="6"]
+
+
+
 
