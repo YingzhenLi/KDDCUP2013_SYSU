b867281c6496623fb6b66f8a91ffc03ee4a5b230
YingzhenLi <liyzhen2@gmail.com>
2013-05-02 21:45:55 +0800

    performing LDA on journal/conferences

---
+++ LDA/journal_conf_keyword_extraction.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+# extracting keywords of some journal/conference
+
+import re
+import json
+import os
+import pickle
+import psycopg2
+from itertools import chain
+
+# global variable
+conn_string = None
+
+# just copy from data_io.py
+def get_paths():
+    paths = json.loads(open("../PythonBenchmark/SETTINGS.json").read())
+    for key in paths:
+        paths[key] = os.path.expandvars(paths[key])
+    return paths
+
+def get_db_conn():
+    # Horrible coding practice. Don't use global variables. Don't follow this example.
+    # Wanted to ask for a password, didn't have time to refactor to be OO.
+    global conn_string
+    if conn_string is None:
+        conn_string = get_paths()["postgres_conn_string"]
+    if "##AskForPassword##" in conn_string:
+        password = raw_input("PostgreSQL Password: ")
+        conn_string = conn_string.replace("##AskForPassword##", password)
+    conn = psycopg2.connect(conn_string)
+    return conn
+
+# my work -- yingzhen
+def print_keywords_to_file(conn, journal_conf_id):
+    # print all the keywords of a journal/conference to a txt file "journal/conference_shortname.txt"   
+    # query = open("journal_conf_keywords.sql").read().strip()
+    query = """
+	    SELECT keyword FROM paper WHERE journalid = ##journal_conf_id##
+	    """
+    query = query.replace("##journal_conf_id##", str(journal_conf_id[0]))
+    cursor = conn.cursor()
+    cursor.execute(query)
+    res = map(list, cursor.fetchall())	# a list of paper keywords
+    pickle.dump(res, open("journal_conf_keyword/" + str(journal_conf_id[0]) + ".pickle", "w"))
+    check = 1	# to be modified
+    return check
+
+def get_journal_conf_id(conn, num, journal_or_conference = "journal"):
+    query = """
+	    SELECT id FROM ##journal_or_conference## LIMIT ##num##
+	    """
+    query = query.replace("##num##", str(num))
+    query = query.replace("##journal_or_conference##", journal_or_conference)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    res = cursor.fetchall()
+    return res
+
+def journal_conf_keyword_generation(conn, num = 0, journal_or_conference = "journal"):
+    # getting journal/conference's shortname and id list (journal_conf_name, journal_conf_id)
+    journal_conf_list = get_journal_conf_id(conn, num, journal_or_conference)
+    # print their keywords to files
+    for journal_conf_id in journal_conf_list:
+	print_keywords_to_file(conn, journal_conf_id)
+    check = 1	# to be modified
+    return check
+
+def input_journal_conf_keywords(journal_conf_list):
+    # input the keyword documents into a list for lda package
+    journal_conf_keyword_list = []
+    for journal_conf_pickle in journal_conf_list:
+	# join to a string
+	keywords = pickle.load(open("journal_conf_keyword/" + journal_conf_pickle))
+	keywords = "".join(chain.from_iterable(keywords))
+	keywords = re.sub('\n', ' ', keywords)
+	journal_conf_keyword_list.append(keywords)
+    return journal_conf_keyword_list
+
+def vocabulary_generation(journal_conf_keyword_list, vocab):
+    # just getting keywords from the phrases
+    for keywords in journal_conf_keyword_list:
+        keywords = keywords.lower()
+        keywords = re.sub(r'[^a-z0-9]', ' ', keywords)	# to be modified!
+	print keywords
+	keyword_set = keywords.split(' ')	# to be modified!
+	for keyword in keyword_set:
+	    # adding new keywords into the vocabulary
+	    if keyword not in vocab:
+		vocab[keyword] = len(vocab.keys()) + 1	# assigning new id 
+    return vocab
---
+++ LDA/journal_lda.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+# simple test of LDA on journals/conferences
+# reuse data_io.py and onlinewikipedia.py functions
+
+import numpy, sys, os
+import onlineldavb as lda
+import journal_conf_keyword_extraction as jcke
+	
+def main():
+    # LDA: a documents contains all the keywords of some journal/conference
+    # equivalent to cluster keywords over journals/conferences
+    journal_or_conference = sys.argv[1]
+    num = int(sys.argv[2])
+    conn = jcke.get_db_conn()
+
+    # default (num <=0 or > max number): figure out all the journals/conferences keywords
+    if num <= 0 or (num >= 15151 and journal_or_conference == "journal") or (num >= 4545 and journal_or_conference == "conference"):
+	query = """
+		SELECT COUNT(*) FROM ##journal_or_conference##
+		"""
+	query = query.replace("##journal_or_conference##", journal_or_conference)
+	conn.cursor.execute(query)
+	num = conn.cursor.fetchall()	# number of journals/conferences to process
+
+    # document parsing
+    journal_conf_list = os.listdir("journal_conf_keyword")
+    # check if txt files exist, then generate those docs
+    if not num == len(journal_conf_list):
+	jcke.journal_conf_keyword_generation(conn, num, journal_or_conference)
+	journal_conf_list = os.listdir("journal_conf_keyword")
+    
+    # The number of journal/conference keyword sets in each batch
+    batchsize = lambda num: num if num <= 100 else 100
+    batch = batchsize(num)
+    iteration_times = int(num/batch)
+    # The total number of journals/conferences
+    D = lambda journal_or_conference: 15151 if journal_or_conference == "journal" else 4545
+    # The number of topics
+    K = 100	# maybe some other numbers
+    # Our vocabulary : we need some vocabulary set!
+    vocab = dict()
+    # Initialize the algorithm with alpha=1/K, eta=1/K, tau_0=1024, kappa=0.7
+    online_LDA = lda.OnlineLDA(vocab, K, D, 1./K, 1./K, 1024., 0.7)
+
+    # Run until we've seen D documents. (Feel free to interrupt *much* sooner than this.)
+    for iteration in range(0, iteration_times):
+	# getting documents (keyword sets)
+	if iteration != iteration_times - 1:
+    	    journal_conf_keyword_list = jcke.input_journal_conf_keywords(journal_conf_list[iteration * batch : (iteration + 1) * batch])
+	else:
+	    journal_conf_keyword_list = jcke.input_journal_conf_keywords(journal_conf_list[iteration * batch :])
+        # online LDA for keyword sets
+	# here we update the relative function in the package	(dangerous!)	
+	print journal_conf_keyword_list
+	online_LDA._vocab = jcke.vocabulary_generation(journal_conf_keyword_list, online_LDA._vocab)	
+        (gamma, bound) = online_LDA.update_lambda(journal_conf_keyword_list)
+        # Compute an estimate of held-out perplexity
+        (keywordids, keywordcts) = lda.parse_doc_list(journal_conf_keyword_list, online_LDA._vocab)
+        perkeywordbound = bound * len(journal_conf_keyword_list) / (D * sum(map(sum, keywordcts)))
+        print '%d:  rho_t = %f,  held-out perplexity estimate = %f' % \
+            (iteration, online_LDA._rhot, numpy.exp(-perkeywordbound))
+
+    # Save lambda, the parameters to the variational distributions over topics, and gamma, the parameters to the variational
+    # distributions over topic weights for the articles analyzed in the last iteration.
+    numpy.savetxt('lambda-%s.dat' % journal_or_conference, online_LDA._lambda)
+    numpy.savetxt('gamma-%s.dat' % journal_or_conference, gamma)
+
+if __name__ == '__main__':
+    main()
