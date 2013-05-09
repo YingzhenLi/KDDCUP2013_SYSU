d8f593827cbae91f892b5901f62e716d11be865c
kaggler <hetong007+kaggle@gmail.com>
2013-05-09 22:52:26 +0800

    Tested text splitter
    Using codes in the last part of DP.R to get the bags and the splitting
    results.

--- R code/GetFeatures/DP.R
+++ R code/GetFeatures/DP.R
@@ -1,9 +1,9 @@
-searchFreq=function(words,sorted.result)
+searchFreq=function(words,wd,freq)
 {
 	word=paste(words,collapse=" ")
 	ind=getind(word,wd)
 	if (is.na(ind))
-		ans=-Inf
+		ans=NA
 	else
 		ans=log(freq[ind])
 	return(ans)
@@ -12,26 +12,32 @@ searchFreq=function(words,sorted.result)
 divSentence=function(sent,wd,freq)#Using log to avoid underflow
 {
 	
-	sent=unlist(strsplit(sent,"[^A-Za-z]"))
-	sent=sent[sent!=""]
+	#sent=unlist(strsplit(sent,"[^A-Za-z]"))
+	#sent=sent[sent!=""]
 	n=length(sent)
+	if (n==1)
+		return(1)
+	mf=min(log(freq))-1
 	f=mat.or.vec(n,n)
 	s=mat.or.vec(n,n)
 	for (i in 1:n)
 	{
 		f[i,i]=searchFreq(sent[i],wd,freq)
+		if (is.na(f[i,i]))
+			f[i,i]=mf#f[i,i]=-Inf
 		s[i,i]=i
 	}
-	for (d in 1:n-1)
+	for (d in 1:(n-1))
 	{
 		for (i in 1:(n-d))
 		{
-			f[i,i+d]=searchFreq(sent[i:i+d],wd,freq)
-			s[i,i+d]=j
+			f[i,i+d]=searchFreq(sent[i:(i+d)],wd,freq)
+			s[i,i+d]=i+d
 			for (k in 0:(d-1))
 			{
 				tmp=f[i,i+k]+f[i+k+1,i+d]
-				if (tmp>f[i,i+d])
+				#show(paste(c(d,i,k),collapse=""))
+				if (is.na(f[i,i+d]) || tmp>f[i,i+d])#Is this standard sufficient enough??
 				{
 					f[i,i+d]=tmp
 					s[i,i+d]=i+k
@@ -44,11 +50,13 @@ divSentence=function(sent,wd,freq)#Using log to avoid underflow
 
 sepSent=function(sent,s,i,j)
 {
+	if (i>j)
+		return("")
 	if (s[i,j]==j)
-		return(sent)
+		return(paste(sent[i:j],collapse=" "))
 	k=s[i,j]
-	p1=sepSent(sent[i:k],s,i,k)
-	p2=sepSent(sent[(k+1):j],s,k+1,j)
+	p1=sepSent(sent,s,i,k)
+	p2=sepSent(sent,s,k+1,j)
 	return(c(p1,p2))
 }
 
@@ -61,13 +69,49 @@ divText=function(txt,result)
 	freq=freq[ind]
 	n=length(txt)
 	ans=list()
+	j=1
 	for (i in 1:n)
 	{
-		tmp=divSentence(txt[i],wd,freq)
-		sent=unlist(strsplit(sent,"[^A-Za-z]"))
+		sent=unlist(strsplit(txt[i],"[^A-Za-z]"))
 		sent=sent[sent!=""]
 		m=length(sent)
-		ans[[i]]=sepSent(sent,s,1,m)
+		if (m==0)
+			ans[[i]]=NULL
+		if (m==1)
+			ans[[i]]=sent
+		else if (m>1)
+		{
+			s=divSentence(sent,wd,freq)
+			ans[[i]]=sepSent(sent,s,1,m)
+		}
 	}
 	return(ans)
 }
+
+SentenceSplit=function(txt,d0=c(1,4),ent0=0,freq0=0,conc0=0)
+{
+	txt=as.character(txt)
+	x=txt
+	x=unlist(strsplit(x,"[^A-Za-z]"))
+	x=x[x!=""]
+	result=PhraseSegment(x,d0=d0,ent0=ent0,freq0=freq0,conc0=conc0)
+	ind=which(txt!="")
+	n=length(txt)
+	ans=rep("",n)
+	txt=txt[txt!=""]
+	a=divText(txt,result)
+	ans[ind]=a
+	return(list(result,ans))
+}
+
+#running on the sample part
+
+#keywords
+tmp=SentenceSplit(paper[,6])#217 secs
+keywords.bag=tmp[[1]]
+keywords.split.result=tmp[[2]]
+
+#title
+tmp=SentenceSplit(paper[,5])#2846 secs
+title.bag=tmp[[1]]
+title.split.result=tmp[[2]]
